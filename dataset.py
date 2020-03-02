f19d5440cbc880e70b438b5fb897be3f7dc039d0
= <=>
2020-03-02 15:07:27 +0100

    Make all set loader

--- dataset.py
+++ dataset.py
@@ -1,14 +1,30 @@
 import sys, os
 import pandas as pd
 
+remu_path = 'data/remu.csv'
+remu_categories = ['ligne_rectification', 'benef_categorie_code', 'benef_qualite_code', 'benef_pays_code', 
+    'benef_titre_code', 'benef_specialite_code', 'benef_identifiant_type_code']
 
-benef_path = 'benef_clean.csv'
+conv_path = 'data/conv.csv'
+conv_categories = ['ligne_rectification', 'benef_categorie_code', 'benef_qualite_code', 'benef_pays_code', 
+    'benef_titre_code', 'benef_specialite_code', 'benef_identifiant_type_code', 'conv_objet']
+
+benef_path = 'data/clean/benef_clean.csv'
 benef_categories = ['ligne_rectification', 'benef_categorie_code', 'benef_qualite_code', 'benef_pays_code', 
     'benef_titre_code', 'benef_specialite_code', 'benef_identifiant_type_code', 'semestre']
 
+
 def load_benef():
-    df = pd.read_csv(benef_path, sep=';', encoding='utf-8')
-    for cat in benef_categories:
+    return load(benef_path, benef_categories)
+
+def load_conv():
+    return load(conv_path, conv_categories)
+
+def load_remu():
+    return load(remu_path, remu_categories)
+
+def load(path, cats):
+    df = pd.read_csv(path, sep=';', encoding='utf-8')
+    for cat in cats:
         df[cat].astype('category')
     return df
-
